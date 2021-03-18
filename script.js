5982f7a3a7e6619e17d2c075eda3327e61f5ee04
p2kr <34896515+p2kr@users.noreply.github.com>
2021-03-18 16:57:48 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+$(function () {
+    $.getJSON("data.json", data => {
+        sessionStorage.setItem("my_data", JSON.stringify(data))
+        console.log("data = ", data);
+        process_data(data);
+    })
+});
+var x; // for testing only
+function process_data(data) {
+    x = data; // for testing only
+
+    var gogostreamData = data["gogo-stream"];
+
+    for (let i = 0; i < gogostreamData.length; i++) {
+        let cdata = gogostreamData[i]; // currentData;
+        let img = "<img class=\"picture img-thumbnail\" src=" + cdata["image"] + "></img>"
+        let name = "<h3>" + cdata["name"] + "</h3>"
+        let time = "<h5>" + cdata["time"] + "</h5>"
+        $("#main-content").append("" + img + name + time + "<hr>");
+    }
+}
